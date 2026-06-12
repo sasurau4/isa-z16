5a1bff1dd2aa7fb8d28bd6d18ac49f61e2c210ce
Daiki Ihara <sasurau4@gmail.com>
2026-06-12 23:09:00 +0900

    feat: add Basic

---
+++ Basic.v
@@ -0,0 +1,20 @@
+module Basic;
+
+  initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, Basic);
+  end
+
+  reg  r_a;
+
+  wire w_x;
+
+  assign w_x = r_a;
+
+  initial begin
+    r_a = 1'b0;
+    #2 r_a = 1'b1;
+    #2 $finish;
+  end
+
+endmodule
