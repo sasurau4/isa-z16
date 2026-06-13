567ec035c29c85d0a810e5b2b94ac175861956b5
Daiki Ihara <sasurau4@gmail.com>
2026-06-13 18:00:31 +0900

    feat: try OR gate

--- Basic.v
+++ Basic.v
@@ -6,14 +6,21 @@ module Basic;
   end
 
   reg  r_a;
+  reg  r_b;
 
   wire w_x;
 
-  assign w_x = ~r_a;
+  assign w_x = r_a | r_b;
 
   initial begin
     r_a = 1'b0;
+    r_b = 1'b0;
+    #2 r_a = 1'b0;
+    r_b = 1'b1;
     #2 r_a = 1'b1;
+    r_b = 1'b0;
+    #2 r_a = 1'b1;
+    r_b = 1'b1;
     #2 $finish;
   end
 
