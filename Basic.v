e8ff1a55bad6dbc5ee627d2fdae845e2245440ed
Daiki Ihara <sasurau4@gmail.com>
2026-06-13 18:22:18 +0900

    feat: try HalfAdder

--- Basic.v
+++ Basic.v
@@ -1,5 +1,6 @@
 module Basic;
 
+  // --- おまじないここから ---
   initial begin
     $dumpfile("wave.vcd");
     $dumpvars(0, Basic);
@@ -7,38 +8,26 @@ module Basic;
 
   reg  r_a;
   reg  r_b;
-  reg  r_s;
+  // --- おまじないここまで ---
 
+  wire w_c;
   wire w_x;
 
-  assign w_x = r_s ? r_b : r_a;
+  assign w_c = r_a & r_b;
+  assign w_s = r_a ^ r_b;
 
+  // --- おまじないここから ---
   initial begin
     r_a = 1'b0;
     r_b = 1'b0;
-    r_s = 1'b0;
-    #2 r_a = 1'b0;
-    r_b = 1'b0;
-    r_s = 1'b1;
     #2 r_a = 1'b0;
     r_b = 1'b1;
-    r_s = 1'b0;
-    #2 r_a = 1'b0;
-    r_b = 1'b1;
-    r_s = 1'b1;
     #2 r_a = 1'b1;
     r_b = 1'b0;
-    r_s = 1'b0;
-    #2 r_a = 1'b1;
-    r_b = 1'b0;
-    r_s = 1'b1;
     #2 r_a = 1'b1;
     r_b = 1'b1;
-    r_s = 1'b0;
-    #2 r_a = 1'b1;
-    r_b = 1'b1;
-    r_s = 1'b1;
     #2 $finish;
   end
+  // --- おまじないここまで ---
 
 endmodule
