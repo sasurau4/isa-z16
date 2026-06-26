12e5c76443beb305451417b3e4cafe29c1735ae3
Daiki Ihara <sasurau4@gmail.com>
2026-06-26 21:33:21 +0900

    feat: impl fetch instr

--- Z16CPU.v
+++ Z16CPU.v
@@ -3,10 +3,23 @@ module Z16CPU (
     input wire i_rst
 );
 
+  // Program Counter
+  reg  [15:0] r_pc;
+  wire [15:0] w_instr;
+
+  always @(posedge i_clk) begin
+    if (i_rst) begin
+      // reset
+      r_pc <= 16'h0000;
+    end else begin
+      r_pc <= r_pc + 16'h0002;
+    end
+  end
+
   // Instruction Memory
   Z16InstrMem InstrMem (
-      .i_addr (),
-      .o_instr()
+      .i_addr (r_pc),    // Address from Program Counter
+      .o_instr(w_instr)
   );
 
   // Data Memory
@@ -17,4 +30,5 @@ module Z16CPU (
       .i_data(),
       .o_data()
   );
+
 endmodule
