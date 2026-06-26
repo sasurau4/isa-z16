60131ed48e145cbbf8c62046ff7126b0497a836b
Daiki Ihara <sasurau4@gmail.com>
2026-06-26 22:09:26 +0900

    feat: impl Decoder

--- Z16CPU.v
+++ Z16CPU.v
@@ -4,8 +4,15 @@ module Z16CPU (
 );
 
   // Program Counter
-  reg  [15:0] r_pc;
+  reg [15:0] r_pc;
+
   wire [15:0] w_instr;
+  wire [15:0] w_rd_addr;  // Wire for destination register address
+  wire [15:0] w_rs1_addr;  // Wire for source register 1 address
+  wire [15:0] w_imm;  // Wire for immediate value
+  wire w_rd_wen;  // Wire for write enable for destination register
+  wire w_mem_wen;  // Wire for write enable for memory
+  wire [3:0] w_alu_ctrl;  // Wire for ALU control signal
 
   always @(posedge i_clk) begin
     if (i_rst) begin
@@ -20,6 +27,16 @@ module Z16CPU (
   Z16InstrMem InstrMem (
       .i_addr (r_pc),    // Address from Program Counter
       .o_instr(w_instr)
+  );
+
+  Z16Decoder Decoder (
+      .i_instr   (w_instr),
+      .o_rd_addr (w_rd_addr),
+      .o_rs1_addr(w_rs1_addr),
+      .o_imm     (w_imm),
+      .o_rd_wen  (w_rd_wen),
+      .o_mem_wen (w_mem_wen),
+      .o_alu_ctrl(w_alu_ctrl)
   );
 
   // Data Memory
---
+++ Z16Decoder.v
@@ -0,0 +1,64 @@
+module Z16Decoder (
+    input wire [15:0] i_instr,  // input instruction
+    output wire [3:0] o_opcode,  // output opcode
+    output wire [3:0] o_rd_addr,  // output destination register address
+    output wire [3:0] o_rs1_addr,  // output source register 1 address
+    output wire [15:0] o_imm,  // output immediate value
+    output wire o_rd_wen,  // output write enable for destination register
+    output wire o_mem_wen,  // output write enable for memory
+    output wire [3:0] o_alu_ctrl  // output ALU control signal
+);
+
+  assign o_opcode = i_instr[3:0];
+  assign o_rd_addr = i_instr[7:4];
+  assign o_rs1_addr = i_instr[11:8];
+  assign o_imm = get_imm(i_instr);
+  assign o_rd_wen = get_rd_wen(i_instr);
+  assign o_mem_wen = get_mem_wen(i_instr);
+  assign o_alu_ctrl = get_alu_ctrl(i_instr);
+
+  // sign-extended immediate value based on opcode
+  function [15:0] get_imm;
+    input [15:0] i_instr;
+    begin
+      case (i_instr[3:0])
+        // Syntax for concatenation: {{number of bits to extend}{sign bit}}, immediate bits
+        4'hA: get_imm = {{12{i_instr[15]}}, i_instr[15:12]};
+        default: get_imm = 16'h0000;
+      endcase
+    end
+  endfunction
+
+  function get_rd_wen;
+    input [15:0] i_instr;
+    begin
+      if (4'hA == i_instr[3:0]) begin
+        get_rd_wen = 1'b1;
+      end else begin
+        get_rd_wen = 1'b0;
+      end
+    end
+  endfunction
+
+  function get_mem_wen;
+    input [15:0] i_instr;
+    begin
+      if (4'hA == i_instr[3:0]) begin
+        get_mem_wen = 1'b0;
+      end else begin
+        get_mem_wen = 1'b0;
+      end
+    end
+  endfunction
+
+  function [3:0] get_alu_ctrl;
+    input [15:0] i_instr;
+    begin
+      if (4'hA == i_instr[3:0]) begin
+        get_alu_ctrl = 4'h0;
+      end else begin
+        get_alu_ctrl = 4'h0;
+      end
+    end
+  endfunction
+endmodule
