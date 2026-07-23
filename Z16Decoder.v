2fbaed89c87dc4cf7717afc80d3a5899f2c3d63c
Daiki Ihara <sasurau4@gmail.com>
2026-07-23 20:16:01 +0900

    feat: impl data path of imm inst to decoder

--- Z16Decoder.v
+++ Z16Decoder.v
@@ -12,12 +12,22 @@ module Z16Decoder (
 
   assign o_opcode = i_instr[3:0];
   assign o_rd_addr = i_instr[7:4];
-  assign o_rs1_addr = i_instr[11:8];
+  assign o_rs1_addr = get_rs1_addr(i_instr);
   assign o_rs2_addr = i_instr[15:12];
   assign o_imm = get_imm(i_instr);
   assign o_rd_wen = get_rd_wen(i_instr);
   assign o_mem_wen = get_mem_wen(i_instr);
   assign o_alu_ctrl = get_alu_ctrl(i_instr);
+
+  function [3:0] get_rs1_addr;
+    input [15:0] i_instr;
+    begin
+      case (i_instr[3:0])
+        4'h9: get_rs1_addr = i_instr[7:4];
+        default: get_rs1_addr = i_instr[11:8];
+      endcase
+    end
+  endfunction
 
   // sign-extended immediate value based on opcode
   function [15:0] get_imm;
@@ -25,6 +35,7 @@ module Z16Decoder (
     begin
       case (i_instr[3:0])
         // Syntax for concatenation: {{number of bits to extend}{sign bit}}, immediate bits
+        4'h9: get_imm = {{8{i_instr[15]}}, i_instr[15:8]};
         4'hA: get_imm = {{12{i_instr[15]}}, i_instr[15:12]};
         4'hB: get_imm = {{12{i_instr[7]}}, i_instr[7:4]};
         default: get_imm = 16'h0000;
@@ -35,9 +46,7 @@ module Z16Decoder (
   function get_rd_wen;
     input [15:0] i_instr;
     begin
-      if (i_instr[3:0] <= 4'h8) begin
-        get_rd_wen = 1'b1;
-      end else if (4'hA == i_instr[3:0]) begin
+      if (i_instr[3:0] <= 4'hA) begin
         get_rd_wen = 1'b1;
       end else begin
         get_rd_wen = 1'b0;
